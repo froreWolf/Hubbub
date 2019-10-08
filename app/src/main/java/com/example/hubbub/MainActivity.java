a4b82f6bd5fd6b0c841fa92b16ae8dab34569889
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2019-10-08 08:46:43 -0400

    created map/login activities
    created activities for login and map screens. Also created an example action litener

--- app/src/main/java/com/example/hubbub/MainActivity.java
+++ app/src/main/java/com/example/hubbub/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.hubbub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickMeButton = findViewById(R.id.clickMeButton);
+        final TextView resultsText = findViewById(R.id.clickMeResults);
+        clickMeButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v){
+                resultsText.setText("Hello There!");
+            }
+        });
+
+
     }
 }
