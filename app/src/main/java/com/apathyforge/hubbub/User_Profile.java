d7a9696575cfe5aae8d182c042026a74285a66f2
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-01-28 12:04:05 -0500

    added key for google maps plugin

--- app/src/main/java/com/apathyforge/hubbub/User_Profile.java
+++ app/src/main/java/com/apathyforge/hubbub/User_Profile.java
@@ -3,12 +3,50 @@ package com.apathyforge.hubbub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class User_Profile extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class User_Profile extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__profile);
+
+        TextView username = findViewById(R.id.profileUserName);
+        TextView email = findViewById(R.id.profileEmail);
+        ImageView pfp = findViewById(R.id.profilePicture);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        username.setText("Name: " + user.getDisplayName());
+        email.setText("Email: " + user.getEmail());
+
+        getUserInfo();
+
+        //get rid of this later please!!!!!
+        updateUserInfo();
     }
+
+
+
+    public void getUserInfo()
+    {
+
+    }
+
+    public void updateUserInfo()
+    {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello World.");
+    }
+
 }
