07ab9e6763ad6d63e20cd72c56f5b90166d0777c
Branden Barber <37583030+froreWolf@users.noreply.github.com>
2020-02-18 08:51:28 -0500

    updated layouts to relative

--- app/src/main/java/com/apathyforge/hubbub/Friends.java
+++ app/src/main/java/com/apathyforge/hubbub/Friends.java
@@ -1,8 +1,11 @@
 package com.apathyforge.hubbub;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Friends extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class Friends extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch(item.getItemId())
+        {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOPtionsMenu(Menu menu)
+    {
+        return true;
     }
 }
